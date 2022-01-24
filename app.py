a2f429658a3e659358b8a08a47acf7923e14ee6a
mbcollins2 <mbcollins2@gmail.com>
2022-01-23 19:00:00 -0500

    Update app.py. Add requirements.txt

--- app.py
+++ app.py
@@ -22,16 +22,37 @@ st.markdown(hide_st_style, unsafe_allow_html=True)
 
 st.title(':basketball:  Dub Dynasty')
 
+# Download Buttons
 b_col_1, b_col_2, _,_,_,_ = st.columns(6)
-
 with b_col_1:
-    st.button('Update League Data', on_click=download_league_data())
+    download_league = st.button('Update League Data')
+    if download_league:
+        download_league_data()
+    # st.write('')
 with b_col_2:
-    st.button('Update Free Agent Data', on_click=download_free_agents())
+    download_fa = st.button('Update Free Agent Data')
+    if download_fa:
+        download_free_agents()
+    # st.write('')
 
+# Free Agent Data
 st.write('')
 st.subheader('Free Agents')
+
 fa = pd.read_csv('free_agents.csv')
+
+# filters
+columns = st.multiselect('Filter Columns', options=fa.columns)
+if columns:
+    fa = fa[columns]
+
+n_col_1, n_col_2, n_col_3, n_col_4, n_col_5, n_col_6 = st.columns(6)
+with n_col_1:
+    min_gp = st.number_input('Min. GP', min_value=0, value=0, step=1)
+
+fa = fa[(fa['total_gp']>=min_gp)]
+
+
 st.dataframe(data=fa)
 
 
